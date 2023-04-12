50d3ceda6c77abb80a24f7f20962a6da2af72c1c
James Miller <jamesedmiller8@gmail.com>
2023-04-11 23:33:37 -0500

    Added loop every 24 hours
    *Note, this is not implementing timing as that would mean that it would run at a specific time and not instantly

--- main.py
+++ main.py
@@ -129,97 +129,98 @@ discord = ds_api.Client(intents=discord_intents)
 async def on_ready():
     # wait = datetime.datetime.today()
     # wait2 = wait.replace(hour=0, minute=34, second=0)
-    # while True:
+    while True:
     #     wait3 = wait2 - wait
     #     if wait3.days == -1:
     #         wait = datetime.datetime.today()
     #         wait2 = wait.replace(minute=wait2.minute+1)
     #         wait3 = wait2 - wait
     #     await asyncio.sleep(wait3.seconds)
-    patreon.codes = []
-    patreon.refresh_pledges()
-    missing_discord = ds_api.Embed(
-        title='Patrons Missing Linked Discord',
-        description='These patrons do not have a Discord Linked, here are their Coupon Codes for Manual Forwarding'
-    )
-    for member in patreon.pledges:
-        print(f'({datetime.datetime.now()}) Generating Coupons for Patron: {member}...')
-        member = patreon.pledges[member]
-        member.disc_codes = {}
-        member.gen_disc_codes()
-        if member.discord_id:
-            member_discord = discord.get_user(member.discord_id)
-            embed = ds_api.Embed(title='Today\'s Coupon Codes!',
-                                 description='Here is a list of coupon codes that you get as a benefit from Patreon'
-                                             '!')
-            for coupon in member.disc_codes:
-                embed.add_field(name=coupon, value=member.disc_codes[coupon])
-                patreon.codes.append({'name': coupon, 'code': member.disc_codes[coupon]})
-            print(f'({datetime.datetime.now()}) Sending Coupons to {member.full_name}\'s Discord ({member_discord.display_name}#'
-                  f'{member_discord.discriminator})...')
-            member_dm = await discord.create_dm(member_discord)
-            await member_dm.send(embed=embed)
-        else:
-            print(f'({datetime.datetime.now()}) Member {member.full_name} Does not have a Discord Linked, Code will be generated anyway and for'
-                  f'warded to Server to be sent Manually')
-            value = ''
-            for coupon in member.disc_codes:
-                value += f'**{coupon}**: {member.disc_codes[coupon]}\n'
-                patreon.codes.append({'name': coupon, 'code': member.disc_codes[coupon]})
-            missing_discord.add_field(
-                name=member.full_name,
-                value=value
-            )
-    await discord.get_guild(1053851544765874216).get_channel(1053858714911776790).send(embed=missing_discord)
-    print(f'({datetime.datetime.now()}) Forwarding Coupons to Server...')
-    embed = ds_api.Embed(
-        title='Today\'s Coupon Codes!',
-        description='These are the codes that can be used for today from Patrons'
-    )
-    ten_pc_off_codes = ''
-    fifteen_pc_off_codes = ''
-    twenty_pc_off_codes = ''
-    thirty_pc_off_codes = ''
-    half_off_admission_codes = ''
-    free_admission_codes = ''
-    for coupon in patreon.codes:
-        if coupon['name'] == '10% Off In-Store Purchases':
-            ten_pc_off_codes += f'{coupon["code"]}\n'
-        if coupon['name'] == '15% Off In-Store Purchases':
-            fifteen_pc_off_codes += f'{coupon["code"]}\n'
-        if coupon['name'] == '20% Off In-Store Purchases':
-            twenty_pc_off_codes += f'{coupon["code"]}\n'
-        if coupon['name'] == '30% Off In-Store Purchases':
-            thirty_pc_off_codes += f'{coupon["code"]}\n'
-        if coupon['name'] == 'Half-Off Admission to Live Shows':
-            half_off_admission_codes += f'{coupon["code"]}\n'
-        if coupon['name'] == 'Free Admission to Live Shows':
-            free_admission_codes += f'{coupon["code"]}\n'
-    embed.add_field(
-        name='10% Off In-Store Purchases',
-        value=ten_pc_off_codes
-    )
-    embed.add_field(
-        name='15% Off In-Store Purchases',
-        value=fifteen_pc_off_codes
-    )
-    embed.add_field(
-        name='20% Off In-Store Purchases',
-        value=twenty_pc_off_codes
-    )
-    embed.add_field(
-        name='30% Off In-Store Purchases',
-        value=thirty_pc_off_codes
-    )
-    embed.add_field(
-        name='Half-Off Admission to Live Shows',
-        value=half_off_admission_codes
-    )
-    embed.add_field(
-        name='Free Admission to Live Shows',
-        value=free_admission_codes
-    )
-    await discord.get_guild(1053851544765874216).get_channel(1053858714911776790).send(embed=embed)
-    print(f'({datetime.datetime.now()}) Done!')
+        patreon.codes = []
+        patreon.refresh_pledges()
+        missing_discord = ds_api.Embed(
+            title='Patrons Missing Linked Discord',
+            description='These patrons do not have a Discord Linked, here are their Coupon Codes for Manual Forwarding'
+        )
+        for member in patreon.pledges:
+            print(f'({datetime.datetime.now()}) Generating Coupons for Patron: {member}...')
+            member = patreon.pledges[member]
+            member.disc_codes = {}
+            member.gen_disc_codes()
+            if member.discord_id:
+                member_discord = discord.get_user(member.discord_id)
+                embed = ds_api.Embed(title='Today\'s Coupon Codes!',
+                                     description='Here is a list of coupon codes that you get as a benefit from Patreon'
+                                                 '!')
+                for coupon in member.disc_codes:
+                    embed.add_field(name=coupon, value=member.disc_codes[coupon])
+                    patreon.codes.append({'name': coupon, 'code': member.disc_codes[coupon]})
+                print(f'({datetime.datetime.now()}) Sending Coupons to {member.full_name}\'s Discord ({member_discord.display_name}#'
+                      f'{member_discord.discriminator})...')
+                member_dm = await discord.create_dm(member_discord)
+                await member_dm.send(embed=embed)
+            else:
+                print(f'({datetime.datetime.now()}) Member {member.full_name} Does not have a Discord Linked, Code will be generated anyway and for'
+                      f'warded to Server to be sent Manually')
+                value = ''
+                for coupon in member.disc_codes:
+                    value += f'**{coupon}**: {member.disc_codes[coupon]}\n'
+                    patreon.codes.append({'name': coupon, 'code': member.disc_codes[coupon]})
+                missing_discord.add_field(
+                    name=member.full_name,
+                    value=value
+                )
+        await discord.get_guild(1053851544765874216).get_channel(1053858714911776790).send(embed=missing_discord)
+        print(f'({datetime.datetime.now()}) Forwarding Coupons to Server...')
+        embed = ds_api.Embed(
+            title='Today\'s Coupon Codes!',
+            description='These are the codes that can be used for today from Patrons'
+        )
+        ten_pc_off_codes = ''
+        fifteen_pc_off_codes = ''
+        twenty_pc_off_codes = ''
+        thirty_pc_off_codes = ''
+        half_off_admission_codes = ''
+        free_admission_codes = ''
+        for coupon in patreon.codes:
+            if coupon['name'] == '10% Off In-Store Purchases':
+                ten_pc_off_codes += f'{coupon["code"]}\n'
+            if coupon['name'] == '15% Off In-Store Purchases':
+                fifteen_pc_off_codes += f'{coupon["code"]}\n'
+            if coupon['name'] == '20% Off In-Store Purchases':
+                twenty_pc_off_codes += f'{coupon["code"]}\n'
+            if coupon['name'] == '30% Off In-Store Purchases':
+                thirty_pc_off_codes += f'{coupon["code"]}\n'
+            if coupon['name'] == 'Half-Off Admission to Live Shows':
+                half_off_admission_codes += f'{coupon["code"]}\n'
+            if coupon['name'] == 'Free Admission to Live Shows':
+                free_admission_codes += f'{coupon["code"]}\n'
+        embed.add_field(
+            name='10% Off In-Store Purchases',
+            value=ten_pc_off_codes
+        )
+        embed.add_field(
+            name='15% Off In-Store Purchases',
+            value=fifteen_pc_off_codes
+        )
+        embed.add_field(
+            name='20% Off In-Store Purchases',
+            value=twenty_pc_off_codes
+        )
+        embed.add_field(
+            name='30% Off In-Store Purchases',
+            value=thirty_pc_off_codes
+        )
+        embed.add_field(
+            name='Half-Off Admission to Live Shows',
+            value=half_off_admission_codes
+        )
+        embed.add_field(
+            name='Free Admission to Live Shows',
+            value=free_admission_codes
+        )
+        await discord.get_guild(1053851544765874216).get_channel(1053858714911776790).send(embed=embed)
+        print(f'({datetime.datetime.now()}) Done!')
+        await asyncio.sleep(86400)
 
 discord.run(os.getenv('DISCORD'))
